9bd7899f800f3c27b932f25e38fa85b1fe40fd89
rogerdpack <rogerpack2005@gmail.com>
2015-10-12 17:56:12 -0600

    compiles the enc woot

---
+++ libavcodec/rzip.h
@@ -0,0 +1,10 @@
+// TODO header here
+
+#ifndef AVCODEC_RZIP_H
+#define AVCODEC_RZIP_H
+
+typedef struct RzipContext {
+} RzipContext ;
+
+
+#endif
---
+++ libavcodec/rzipenc.c
@@ -0,0 +1,58 @@
+#include "rzip.h"
+#include "avcodec.h" // AV_CODEC_CAP_INTRA_ONLY
+#include "libavutil/opt.h" // AVOption
+#include "internal.h" // AV_CODEC_CAP_FRAME_THREADS
+
+
+static const AVOption options[] = {
+    { NULL },
+};
+
+static const AVClass class = {
+    .class_name = "rzip",
+    .item_name  = av_default_item_name,
+    .option     = options,
+    .version    = LIBAVUTIL_VERSION_INT,
+};
+
+static av_cold int encode_init(AVCodecContext *avctx)
+{
+    RzipContext *s = avctx->priv_data;
+    // ...
+
+    return 0;
+}
+
+static int encode_frame(AVCodecContext *avctx, AVPacket *pkt,
+                        const AVFrame *pict, int *got_packet)
+{
+    RzipContext *s = avctx->priv_data;
+    // ...
+    return 0;
+}
+
+static av_cold int encode_end(AVCodecContext *avctx)
+{
+    RzipContext *s = avctx->priv_data;
+    return 0;
+}
+
+AVCodec ff_rzip_encoder = {
+    .name           = "rzip",
+    .long_name      = NULL_IF_CONFIG_SMALL("RZIP [Roger Zip] Lossless"),
+    .type           = AVMEDIA_TYPE_VIDEO,
+    .id             = AV_CODEC_ID_RZIP,
+    .priv_data_size = sizeof(RzipContext),
+    .init           = encode_init,
+    .encode2        = encode_frame,
+    .close          = encode_end,
+    .capabilities   = AV_CODEC_CAP_FRAME_THREADS | AV_CODEC_CAP_INTRA_ONLY,
+    .priv_class     = &class,
+    .pix_fmts       = (const enum AVPixelFormat[]){
+        //AV_PIX_FMT_YUV422P, AV_PIX_FMT_RGB32, AV_PIX_FMT_NONE
+        AV_PIX_FMT_RGB24
+    },
+    .caps_internal  = FF_CODEC_CAP_INIT_THREADSAFE |
+                      FF_CODEC_CAP_INIT_CLEANUP,
+};
+
