074c586a70cbd2c39af53b2c94979389c764845f
rogerdpack <rogerpack2005@gmail.com>
2015-12-17 06:37:56 -0700

    batch

--- gof.bat
+++ gof.bat
@@ -1,4 +1,5 @@
 @rem make ffmpeg_g.exe 
 echo "using remote ffmpeg.exe"
-c:\Users\rdp\Downloads\ffmpeg.exe -f dshow -list_devices 1 -dtv 4 -i x
+@rem c:\Users\rdp\Downloads\ffmpeg.exe -f dshow -list_devices 1 -dtv 4 -i x
+del yo.mp4
 c:\Users\rdp\Downloads\ffmpeg.exe -debug 1 -loglevel verbose -f dshow -receiver_component "Hauppauge WinTV 885 TS Capture" -tune_freq 651250 -dtv 4 -dump_dtv_graph dump.grf -i video="Hauppauge WinTV 885 BDA Tuner/Demod" -t 0.1 -y yo.mp4
