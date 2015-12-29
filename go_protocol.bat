5be6bd839e71df9e025a0a548d075cec386f4c98
rogerdpack <rogerpack2005@gmail.com>
2015-12-29 07:16:55 -0700

    huh

--- go_protocol.bat
+++ go_protocol.bat
@@ -5,4 +5,4 @@ del yo.mp4
 del myfile
 "C:\Program Files\Git\usr\bin\gzip.exe" -f -d c:\Users\rdp\Downloads\ffmpeg.exe.gz 
 @rem -analyzeduration 1G -probesize 1G
-c:\Users\rdp\Downloads\ffmpeg.exe -local_buffer_size 500M -debug 1 -loglevel info -receiver_component "Hauppauge WinTV 885 TS Capture" -tune_freq 651250 -dtv a -dump_dtv_graph dump.grf -i dshowbda:video="Hauppauge WinTV 885 BDA Tuner/Demod" -t 20 -y yo.mp4
+c:\Users\rdp\Downloads\ffmpeg.exe -local_buffer_size 500M -debug 1 -loglevel debug -receiver_component "Hauppauge WinTV 885 TS Capture" -tune_freq 651250 -dtv a -dump_dtv_graph dump.grf -i dshowbda:video="Hauppauge WinTV 885 BDA Tuner/Demod" -t 20 -y yo.mp4
