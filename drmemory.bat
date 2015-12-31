577832a1777cba2a5168e85ec00887d28aa8380c
rogerdpack <rogerpack2005@gmail.com>
2015-12-31 11:07:21 -0700

    dr memory, ksl

---
+++ drmemory.bat
@@ -0,0 +1,4 @@
+"c:\Program Files (x86)\Dr. Memory\bin\drmemory.exe" -- .\ffmpeg_g.exe -debug 1 -loglevel info -receiver_component "Hauppauge WinTV 885 TS Capture" -tune_freq 615250 -dtv a -dump_dtv_graph dump.grf -f dshow -i video="Hauppauge WinTV 885 BDA Tuner/Demod" -y yo.mp4
+
+@rem byutv 651250
+@rem ksl 615250
