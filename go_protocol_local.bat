fac2812542ef3b327e0b126677f24c858b2e0761
rogerdpack <rogerpack2005@gmail.com>
2016-01-01 01:02:44 -0700

    make verbose log a lot less again hopefully FTW LOL

--- go_protocol_local.bat
+++ go_protocol_local.bat
@@ -2,9 +2,9 @@
 del yo.mp4
 echo "this does not doa  rebuild"
 del myfile
-del ksl.ts
+del ksl.2hr.ts
 @rem -analyzeduration 1G -probesize 1G
-.\ffmpeg_g.exe -local_buffer_size 500M -debug 1 -loglevel info -receiver_component "Hauppauge WinTV 885 TS Capture" -tune_freq 651250 -dtv a -dump_dtv_graph dump.grf -dump_raw_bytes_file ksl.2hr.ts -i dshowbda:video="Hauppauge WinTV 885 BDA Tuner/Demod" -t 7200 -f null out.null -loglevel verbose
+.\ffmpeg_g.exe -local_buffer_size 500M -debug 1 -loglevel info -receiver_component "Hauppauge WinTV 885 TS Capture" -tune_freq 615250 -dtv a -dump_dtv_graph dump.grf -dump_raw_bytes_file ksl.2hr.ts -i dshowbda:video="Hauppauge WinTV 885 BDA Tuner/Demod" -t 7200 -f null out.null -loglevel debug
 
 @rem byutv 651250
 @rem ksl 615250
