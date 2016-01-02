7947a27938364ac1cc79d658ce09e8a6746924ba
rogerdpack <rogerpack2005@gmail.com>
2016-01-02 03:41:36 -0700

    less logging

--- go_protocol_local.bat
+++ go_protocol_local.bat
@@ -4,7 +4,7 @@ echo "this does not doa  rebuild"
 del myfile
 del ksl.2hr.ts
 @rem -analyzeduration 1G -probesize 1G
-.\ffmpeg_g.exe -local_buffer_size 500M -debug 1 -loglevel info -receiver_component "Hauppauge WinTV 885 TS Capture" -tune_freq 615250 -dtv a -dump_dtv_graph dump.grf -dump_raw_bytes_file ksl.2hr.ts -i dshowbda:video="Hauppauge WinTV 885 BDA Tuner/Demod" -t 7200 -f null out.null -loglevel debug
+.\ffmpeg_g.exe -local_buffer_size 500M -debug 1 -loglevel info -receiver_component "Hauppauge WinTV 885 TS Capture" -tune_freq 615250 -dtv a -dump_dtv_graph dump.grf -dump_raw_bytes_file ksl.2hr.ts -i dshowbda:video="Hauppauge WinTV 885 BDA Tuner/Demod" -t 7200 -f null out.null -loglevel verbose
 
 @rem byutv 651250
 @rem ksl 615250
