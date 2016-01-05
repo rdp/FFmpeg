95f6b9a85af2ad9ac491aa2dbb5a18b791933e76
rogerdpack <rogerpack2005@gmail.com>
2016-01-05 07:48:03 -0700

    more smi

--- smi.bat
+++ smi.bat
@@ -1,1 +1,3 @@
-make ffmpeg_g.exe && .\ffmpeg_g -f dshow -dump_graph_filename smi2.grf -i video="SMI Grabber Device":audio="SMI Grabber Device" -y smi.mp4
+ffmpeg_g -list_options 1 -f dshow -i video="SMI Grabber Device"
+ffmpeg_g -f dshow -i video="SMI Grabber Device":audio="SMI Grabber Device" -y smi.mp4
+ffmpeg_g  -f dshow -crossbar_video_input_pin_number 2 -crossbar_audio_input_pin_number 5 -i video="SMI Grabber Device":audio="SMI Grabber Device" -y out.mp4
