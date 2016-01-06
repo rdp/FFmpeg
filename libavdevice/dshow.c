a9dd671854645c85fe8b38a5616554a86a36c04e
rogerdpack <rogerpack2005@gmail.com>
2016-01-05 18:35:50 -0700

    bring in line with more functional tuner.h

--- libavdevice/dshow.c
+++ libavdevice/dshow.c
@@ -29,12 +29,10 @@
 #include "libavcodec/raw.h"
 #include "objidl.h"
 #include "shlwapi.h"
+#include "bdamedia.h" // KSDATAFORMAT...
 #include "libavformat/url.h"
 #include "libavutil/avstring.h" // avstrstart
 #include "libavutil/avassert.h"
-
-static const GUID KSDATAFORMAT_SUBTYPE_BDA_MPEG2_TRANSPORT_LOCAL =
-    {0xf4aeb342,0x0329,0x4fdd,{0xa8,0xfd,0x4a,0xff,0x49,0x26,0xc9,0x78}};
 
 static enum AVPixelFormat dshow_pixfmt(DWORD biCompression, WORD biBitCount)
 {
@@ -1064,7 +1062,7 @@ dshow_add_device(AVFormatContext *avctx,
           //AVMEDIA_TYPE_VIDEO AV_CODEC_ID_MPEG2VIDEO
           is_mpeg = 1; // NB this has biWidth set but its actually "wrong" or "could be overriden" or the like <sigh>
           av_log(avctx, AV_LOG_ERROR, "got mpeg2video output, which is unsupported currently, ask about it please");
-        } else if (IsEqualGUID(&type.subtype, &KSDATAFORMAT_SUBTYPE_BDA_MPEG2_TRANSPORT_LOCAL)) {
+        } else if (IsEqualGUID(&type.subtype, &KSDATAFORMAT_SUBTYPE_BDA_MPEG2_TRANSPORT)) {
             if (ctx->protocol_av_format_context == NULL) {
               av_log(avctx, AV_LOG_ERROR, "got raw MPEG2 TS stream without being in a urlprotocol context, please rerun like dshowbda:video= instead...");
               ret = AVERROR(EIO);
