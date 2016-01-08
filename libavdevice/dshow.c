a29f2bcea634d642f8e3a12cb466bef9b37545cb
rogerdpack <rogerpack2005@gmail.com>
2016-01-07 19:49:14 -0700

    rename options

--- libavdevice/dshow.c
+++ libavdevice/dshow.c
@@ -1401,7 +1401,7 @@ int dshow_read_packet(AVFormatContext *s, AVPacket *pkt)
 
 #define OFFSET(x) offsetof(struct dshow_ctx, x)
 #define DEC AV_OPT_FLAG_DECODING_PARAM
-static const AVOption options[] = {
+static const AVOption dshow_options[] = {
     { "video_size", "set video size given a string such as 640x480 or hd720.", OFFSET(requested_width), AV_OPT_TYPE_IMAGE_SIZE, {.str = NULL}, 0, 0, DEC },
     { "pixel_format", "set video pixel format", OFFSET(pixel_format), AV_OPT_TYPE_PIXEL_FMT, {.i64 = AV_PIX_FMT_NONE}, -1, INT_MAX, DEC },
     { "framerate", "set video frame rate", OFFSET(framerate), AV_OPT_TYPE_STRING, {.str = NULL}, 0, 0, DEC },
@@ -1446,7 +1446,7 @@ static const AVOption options[] = {
 const AVClass dshow_class = {
     .class_name = "dshow indev",
     .item_name  = av_default_item_name,
-    .option     = options,
+    .option     = dshow_options,
     .version    = LIBAVUTIL_VERSION_INT,
     .category   = AV_CLASS_CATEGORY_DEVICE_VIDEO_INPUT,
 };
